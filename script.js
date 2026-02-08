5ce22114d14617b60f3c92edf5932cb2678cc9d3
Koena Sarkar <koenasarkar04@gmail.com>
2026-02-08 14:24:10 +0530

    Register Controls added

--- script.js
+++ script.js
@@ -2,6 +2,44 @@ let loginForm = document.querySelector("#loginForm");
 let emailInput = document.querySelector("#email");
 let passwordInput = document.querySelector("#password");
 let errorMsg = document.querySelector("#errorMsg");
+let nameInput = document.querySelector("#name");
+let registerForm = document.querySelector("#registerForm");
+
+if (registerForm) {
+    registerForm.addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent form from submitting
+
+        let name = nameInput.value.trim();
+        let email = emailInput.value.trim();
+        let password = passwordInput.value.trim();
+        
+        errorMsg.innerText = ""; // Clear previous error message
+
+        if (name === "" || email === "" || password ==="") {
+            errorMsg.innerText = "All fields are required";
+            return;
+        }
+        if (!email.includes("@") || !email.includes(".")) {
+            errorMsg.innerText = "Enter a valid email address";
+            return;
+        }
+        if (password.length < 8) {
+            errorMsg.innerText = ("Password must be at least 8 characters long");
+            return;
+        }
+        if (localStorage.getItem("user")) {
+            errorMsg.innerText = "User already exists. Please login.";
+            return;
+        }
+        let user = {
+            name : name,
+            email : email,
+            password : password
+        };
+        localStorage.setItem("user", JSON.stringify(user)); // Store user data as a string in local storage
+        window.location.href = "index.html";
+    });
+}
 
 if (window.location.pathname.includes("dashboard.html") && localStorage.getItem("isLoggedIn") !== "true") {
     window.location.href="index.html";
@@ -46,10 +84,3 @@ if (loginForm) {
         window.location.href = "dashboard.html";
     });
 }
-
-let registerForm = document.querySelector("#registerForm");
-
-if (window.location.pathname.includes(".html") && localStorage.getItem("isRegistered") !== "true") {
-    window.location.href="register.html";
-}
-if (registerForm) {
